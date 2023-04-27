08ea28ad5b6a231c9af35b11b4d60ccd2e4e20db
Melissa Castro <m.castros@uniandes.edu.co>
2023-04-27 01:35:31 -0500

    Artist viewModel, list view y fragment

--- BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/models/Performer.kt
+++
@@ -1,8 +0,0 @@
-package co.edu.uniandes.misw4203.group18.backvynils.models
-
-abstract class Performer(
-    open val performerId: Int,
-    open val name: String,
-    open val image: String,
-    open val description: String
-)
--- BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/network/ArtistServiceAdapter.kt
+++ BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/network/ArtistServiceAdapter.kt
@@ -1,46 +1,45 @@
 package co.edu.uniandes.misw4203.group18.backvynils.network
 
 import android.content.Context
-import co.edu.uniandes.misw4203.group18.backvynils.models.Band
-import co.edu.uniandes.misw4203.group18.backvynils.models.Musician
+import co.edu.uniandes.misw4203.group18.backvynils.models.Artist
 import com.android.volley.VolleyError
 import org.json.JSONArray
 
-class PerformerServiceAdapter constructor() {
+class ArtistServiceAdapter constructor() {
     companion object {
-        private var instance: PerformerServiceAdapter? = null
+        private var instance: ArtistServiceAdapter? = null
         fun getInstance() =
             instance ?: synchronized(this) {
-                instance ?: PerformerServiceAdapter().also {
+                instance ?: ArtistServiceAdapter().also {
                     instance = it
                 }
             }
     }
 
     private val bandPath: String = "bands"
-    private val musicianPath: String = "bands"
+    private val musicianPath: String = "musicians"
 
     fun getBands(
         context: Context,
-        onComplete: (resp: List<Band>) -> Unit,
+        onComplete: (resp: List<Artist>) -> Unit,
         onError: (error: VolleyError) -> Unit
     ) {
         VolleyServiceBroker.getInstance(context).getRequest(
             bandPath,
             { response ->
                 val resp = JSONArray(response)
-                val list = mutableListOf<Band>()
+                val list = mutableListOf<Artist>()
 
                 for (i in 0 until resp.length()) {
                     val item = resp.getJSONObject(i)
                     list.add(
                         i,
-                        Band(
-                            performerId = item.getInt("id"),
+                        Artist(
+                            artistId = item.getInt("id"),
                             name = item.getString("name"),
                             image = item.getString("image"),
                             description = item.getString("description"),
-                            creationDate = item.getString("creationDate")
+                            date = item.getString("creationDate")
                         )
                     )
                 }
@@ -53,25 +52,25 @@ class PerformerServiceAdapter constructor() {
 
     fun getMusicians(
         context: Context,
-        onComplete: (resp: List<Musician>) -> Unit,
+        onComplete: (resp: List<Artist>) -> Unit,
         onError: (error: VolleyError) -> Unit
     ) {
         VolleyServiceBroker.getInstance(context).getRequest(
             musicianPath,
             { response ->
                 val resp = JSONArray(response)
-                val list = mutableListOf<Musician>()
+                val list = mutableListOf<Artist>()
 
                 for (i in 0 until resp.length()) {
                     val item = resp.getJSONObject(i)
                     list.add(
                         i,
-                        Musician(
-                            performerId = item.getInt("id"),
+                        Artist(
+                            artistId = item.getInt("id"),
                             name = item.getString("name"),
                             image = item.getString("image"),
                             description = item.getString("description"),
-                            birthDate = item.getString("birthDate")
+                            date = item.getString("birthDate")
                         )
                     )
                 }
