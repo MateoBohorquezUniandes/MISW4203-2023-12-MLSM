a5b62ba43f9e16bc070063d5cee8da9803f5cd6a
Lara Simonetti <l.simonetti@uniandes.edu.co>
2023-04-29 18:52:27 -0700

    Update AlbumCreateFragment.kt

--- BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/ui/AlbumCreateFragment.kt
+++ BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/ui/AlbumCreateFragment.kt
@@ -39,8 +39,6 @@ class AlbumCreateFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
