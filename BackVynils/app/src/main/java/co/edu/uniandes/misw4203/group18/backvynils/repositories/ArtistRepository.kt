154ad13da118ab85f7372f6372dec09e51e28d23
Melissa Castro <m.castros@uniandes.edu.co>
2023-05-14 23:03:46 -0500

    artistas  micro optimizations (#83)
    * add coroutines for musiscians
    
    * added room and some daos
    
    * added tests e2e for artist list
    
    * micro-optimizations

--- BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/repositories/ArtistRepository.kt
+++ BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/repositories/ArtistRepository.kt
@@ -7,15 +7,11 @@ import co.edu.uniandes.misw4203.group18.backvynils.database.dao.ArtistsDao
 import co.edu.uniandes.misw4203.group18.backvynils.models.Artist
 import co.edu.uniandes.misw4203.group18.backvynils.network.ArtistServiceAdapter
 
-import com.android.volley.VolleyError
 
 class ArtistRepository (private val application: Application, private val artistsDao: ArtistsDao){
-    fun updateBandData(onCompleted: (List<Artist>)->Unit, onError: (VolleyError)->Unit) {
-        ArtistServiceAdapter.getInstance().getBands(application, { onCompleted(it) }, onError)
-    }
 
     suspend fun updateMusicianData() : List<Artist>{
-        var cached = artistsDao.getArtists()
+        val cached = artistsDao.getArtists()
         return if(cached.isNullOrEmpty()){
             val cm = application.baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             if( cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI && cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_MOBILE){
