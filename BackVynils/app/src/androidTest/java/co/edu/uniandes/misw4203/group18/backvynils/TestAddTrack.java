c112209e00a604af32912af64a1ad194e4187f49
Lara-Simonetti <the.lemonetti@gmail.com>
2023-05-27 18:10:28 -0700

    Se arreglan las pruebas de HU08

--- BackVynils/app/src/androidTest/java/co/edu/uniandes/misw4203/group18/backvynils/TestAddTrack.java
+++ BackVynils/app/src/androidTest/java/co/edu/uniandes/misw4203/group18/backvynils/TestAddTrack.java
@@ -1,8 +1,13 @@
 package co.edu.uniandes.misw4203.group18.backvynils;
 
 import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -10,6 +15,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import co.edu.uniandes.misw4203.group18.backvynils.ui.AddTrackFragment;
+import co.edu.uniandes.misw4203.group18.backvynils.ui.AlbumListFragment;
 import co.edu.uniandes.misw4203.group18.backvynils.ui.MainActivity;
 
 import org.junit.Before;
@@ -17,9 +23,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -30,24 +41,22 @@ public class TestAddTrack {
 
     @Before
     public void setUp() {
-        // Launch the AddTrackFragment
-        Bundle bundle = new Bundle();
-        bundle.putInt(AddTrackFragment.ALBUM_ID, 100);
-        mActivityTestRule.getScenario().onActivity(activity -> {
-            AddTrackFragment fragment = new AddTrackFragment();
-            fragment.setArguments(bundle);
-            activity.getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
-        });
+        ActivityScenario.launch(MainActivity.class);
+
+        onView(withText("Buscando América")).perform(click());
+
+        onView(withId(R.id.addTrackButton)).perform(click());
     }
+
 
     @Test
     public void testAddTrack_LongName() {
         // Type track name and duration
-        Espresso.onView(ViewMatchers.withId(R.id.nameEditText)).perform(typeText("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean ma"), closeSoftKeyboard());
-        Espresso.onView(ViewMatchers.withId(R.id.durationEditText)).perform(typeText("03:30"), closeSoftKeyboard());
+        onView(withId(R.id.nameEditText)).perform(typeText("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean ma"), closeSoftKeyboard());
+        onView(withId(R.id.durationEditText)).perform(typeText("03:30"), closeSoftKeyboard());
 
         // Click create button
-        Espresso.onView(ViewMatchers.withId(R.id.createButton)).perform(click());
+        onView(withId(R.id.createButton)).perform(click());
 
         try {
             Thread.sleep(500);
@@ -56,19 +65,19 @@ public class TestAddTrack {
         }
 
         // Verify toast message
-        Espresso.onView(ViewMatchers.withText("Please enter a track title with less than 100 characters"))
+        onView(withText("Please enter a track title with less than 100 characters"))
                 .inRoot(ToastMatcher.isToast())
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(isDisplayed()));
     }
 
     @Test
     public void testAddTrack_BadDurationFormat() {
         // Type track name and duration
-        Espresso.onView(ViewMatchers.withId(R.id.nameEditText)).perform(typeText("Test Track"), closeSoftKeyboard());
-        Espresso.onView(ViewMatchers.withId(R.id.durationEditText)).perform(typeText("Duration"), closeSoftKeyboard());
+        onView(withId(R.id.nameEditText)).perform(typeText("Test Track"), closeSoftKeyboard());
+        onView(withId(R.id.durationEditText)).perform(typeText("Duration"), closeSoftKeyboard());
 
         // Click create button
-        Espresso.onView(ViewMatchers.withId(R.id.createButton)).perform(click());
+        onView(withId(R.id.createButton)).perform(click());
 
         try {
             Thread.sleep(500);
@@ -77,19 +86,19 @@ public class TestAddTrack {
         }
 
         // Verify toast message
-        Espresso.onView(ViewMatchers.withText("Please enter the duration in the format MM:SS"))
+        onView(withText("Please enter the duration in the format MM:SS"))
                 .inRoot(ToastMatcher.isToast())
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(isDisplayed()));
 
     }
 
     @Test
     public void testAddTrack_NoName() {
         // Type duration
-        Espresso.onView(ViewMatchers.withId(R.id.durationEditText)).perform(typeText("03:30"), closeSoftKeyboard());
+        onView(withId(R.id.durationEditText)).perform(typeText("03:30"), closeSoftKeyboard());
 
         // Click create button
-        Espresso.onView(ViewMatchers.withId(R.id.createButton)).perform(click());
+        onView(withId(R.id.createButton)).perform(click());
 
         try {
             Thread.sleep(500);
@@ -98,18 +107,18 @@ public class TestAddTrack {
         }
 
         // Verify toast message
-        Espresso.onView(ViewMatchers.withText("Please make sure all fields have been filled correctly"))
+        onView(withText("Please make sure all fields have been filled correctly"))
                 .inRoot(ToastMatcher.isToast())
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(isDisplayed()));
     }
 
     @Test
     public void testAddTrack_NoDuration() {
         // Type track name
-        Espresso.onView(ViewMatchers.withId(R.id.nameEditText)).perform(typeText("Test Track"), closeSoftKeyboard());
+        onView(withId(R.id.nameEditText)).perform(typeText("Test Track"), closeSoftKeyboard());
 
         // Click create button
-        Espresso.onView(ViewMatchers.withId(R.id.createButton)).perform(click());
+        onView(withId(R.id.createButton)).perform(click());
 
         try {
             Thread.sleep(500);
@@ -118,9 +127,9 @@ public class TestAddTrack {
         }
 
         // Verify toast message
-        Espresso.onView(ViewMatchers.withText("Please make sure all fields have been filled correctly"))
+        onView(withText("Please make sure all fields have been filled correctly"))
                 .inRoot(ToastMatcher.isToast())
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(isDisplayed()));
 
     }
 
