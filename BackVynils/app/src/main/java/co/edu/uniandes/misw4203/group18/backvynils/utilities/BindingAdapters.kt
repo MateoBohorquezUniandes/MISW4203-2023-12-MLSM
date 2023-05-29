dd0a6586ba6b749d7a6de067976857c310d5c037
Melissa Castro <m.castros@uniandes.edu.co>
2023-05-28 19:54:40 -0500

    glide cache

--- BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/utilities/BindingAdapters.kt
+++ BackVynils/app/src/main/java/co/edu/uniandes/misw4203/group18/backvynils/utilities/BindingAdapters.kt
@@ -4,13 +4,21 @@ import android.annotation.SuppressLint
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import co.edu.uniandes.misw4203.group18.backvynils.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import java.text.SimpleDateFormat
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
     Glide.with(view.context)
         .load(imageUrl)
+        .apply(
+            RequestOptions()
+            .placeholder(R.drawable.loading_animation)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .error(R.drawable.ic_broken_image))
         .into(view)
 }
 
